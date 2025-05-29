2bc74537ff0cbf2501409b591102e85e4875475e
Linfer7892 <wnsehd06@g.skku.edu>
2025-05-29 09:10:41 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,9 +5,9 @@ def load_model(model_name, num_classes=10) :
     if model_name == "resnet34" :
         return ResNet34(num_classes=num_classes)
     elif model_name == "resnet50" :
-        return ResNet50(num_classes=10)
+        return ResNet50(num_classes=num_classes)
     elif model_name == "preactresnet" :
-        return PreActResNet(num_classes=10)
+        return PreActResNet(num_classes=num_classes)
     elif model_name == "densenet" :
         pass
     elif model_name == "fractalnet" :
