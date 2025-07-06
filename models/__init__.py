2f8332c9a7fff21be213f83cbd50b3313161484b
Linfer7892 <wnsehd06@g.skku.edu>
2025-07-06 20:10:35 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,29 @@
-from .ResNet import ResNet34, ResNet50
-from .PreActResNet import PreActResNet
+from .resnet import resnet18, resnet34, resnet50
+from .preactresnet import preactresnet18
+from .densenet import DenseNet
+from .fractalnet import FractalNet
 
-def load_model(model_name, num_classes=10) :
-    if model_name == "resnet34" :
-        return ResNet34(num_classes=num_classes)
-    elif model_name == "resnet50" :
-        return ResNet50(num_classes=num_classes)
-    elif model_name == "preactresnet" :
-        return PreActResNet(num_classes=num_classes)
-    elif model_name == "densenet" :
-        pass
-    elif model_name == "fractalnet" :
-        pass
+def load_model(model_name, data_name):
+    data_name = data_name.lower()
+    if data_name == "cifar100":
+        num_classes = 100
+    elif data_name == "cifar10":
+        num_classes = 10
+    else:
+        num_classes = 10
+        
+    model_name = model_name.lower()
+    if model_name == "resnet18":
+        return resnet18(num_classes=num_classes)
+    elif model_name == "resnet34":
+        return resnet34(num_classes=num_classes)
+    elif model_name == "resnet50":
+        return resnet50(num_classes=num_classes)
+    elif model_name == "preactresnet18":
+        return preactresnet18(num_classes=num_classes)
+    elif model_name == "densenet":
+        return DenseNet(num_classes=num_classes)
+    elif model_name == "fractalnet":
+        return FractalNet(num_classes=num_classes)
+    else:
+        raise ValueError(f"Model '{model_name}' is not supported.")
