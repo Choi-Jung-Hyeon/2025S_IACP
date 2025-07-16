b9b97e77fe3bfca920e240067af6de1144ac8240
Linfer7892 <wnsehd06@g.skku.edu>
2025-07-16 18:26:28 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,35 @@
-from .resnet34 import resnet34
-from .densenet import DenseNet
-from .fractalnet import FractalNet
-from .preactresnet import PreActResNet
+from .ResNet import ResNet18, ResNet34, ResNet50
+from .DenseNet import DenseNet
+from .FractalNet import FractalNet
+from .PreActResNet import PreActResNet18
+from .ViT import ViT
 from .rotnet import rotnet
 
-def load_model(model_name, num_classes=10):
+
+def load_model(model_name, data_name):
+    data_name = data_name.lower()
+    if data_name == "cifar100":
+        num_classes = 100
+    elif data_name == "cifar10":
+        num_classes = 10
+    else:
+        num_classes = 10
+
     model_name = model_name.lower()
-    if model_name == "resnet34":
-        return resnet34(num_classes=num_classes)
-    elif model_name == "preactresnet":
-        return PreActResNet(num_classes=num_classes)
+    if model_name == "resnet18":
+        return ResNet18(num_classes=num_classes)
+    elif model_name == "resnet34":
+        return ResNet34(num_classes=num_classes)
+    elif model_name == "resnet50":
+        return ResNet50(num_classes=num_classes)
+    elif model_name == "preactresnet18":
+        return PreActResNet18(num_classes=num_classes)
     elif model_name == "densenet":
         return DenseNet(num_classes=num_classes)
     elif model_name == "fractalnet":
         return FractalNet(num_classes=num_classes)
+    elif model_name == "vit":
+        return ViT(num_classes=num_classes)
     elif model_name == "rotnet":
         return rotnet(num_classes=num_classes)
     else:
