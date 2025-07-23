9a04f7ae53a07ee6a56541ad59ef00c3cfeea089
Linfer7892 <wnsehd06@g.skku.edu>
2025-07-24 03:38:57 +0900

    Update PreActResNet.py

--- models/PreActResNet.py
+++ models/PreActResNet.py
@@ -1,62 +1,77 @@
 import torch
 import torch.nn as nn
-from .resnet import ResNet
 
 class PreActBlock(nn.Module):
-    expansion = 1
-    
+
     def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        self.bn1 = nn.BatchNorm2d(in_channels)
-        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, stride, 1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False)
-        
+
+        self.residual_block = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        )
+
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Conv2d(in_channels, out_channels, 1, stride, bias=False)
-    
-    def forward(self, x):
-        out = torch.relu(self.bn1(x))
-        shortcut = self.shortcut(out)
-        out = self.conv1(out)
-        out = self.conv2(torch.relu(self.bn2(out)))
-        return out + shortcut
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+            )
 
-class PreActResNet(ResNet):
-    def __init__(self, block, layers, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_channels = 16
-        
-        self.conv1 = nn.Conv2d(3, 16, 3, 1, 1, bias=False)
-        
-        self.layer1 = self._make_layer(block, 16, layers[0], 1)
-        self.layer2 = self._make_layer(block, 32, layers[1], 2)
-        self.layer3 = self._make_layer(block, 64, layers[2], 2)
-        
-        self.bn = nn.BatchNorm2d(64 * block.expansion)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(64 * block.expansion, num_classes)
-    
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = torch.relu(self.bn(x))
+        return self.residual_block(x) + self.shortcut(x)
+
+
+class PreActResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super().__init__()
+        self.in_channels = 64
+
+        self.stem = nn.Sequential(
+            nn.Conv2d(3, self.in_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        )
+        
+        self.conv2 = self._make_layer(block, 64, layers[0], stride=1)
+        self.conv3 = self._make_layer(block, 128, layers[1], stride=2)
+        self.conv4 = self._make_layer(block, 256, layers[2], stride=2)
+        self.conv5 = self._make_layer(block, 512, layers[3], stride=2)
+        
+        self.classifier = nn.Sequential(
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(512, num_classes)
+        )
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for s in strides:
+            layers.append(block(self.in_channels, out_channels, s))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.stem(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.classifier(x)
+        return x
+        
+    def _extract_features(self, x):
+        x = self.stem(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
-        x = self.fc(x)
         return x
-    
-    def _extract_features(self, x):
-        x = self.conv1(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = torch.relu(self.bn(x))
-        x = self.avgpool(x)
-        return torch.flatten(x, 1)
 
 def preactresnet(num_classes=10):
-    return PreActResNet(PreActBlock, [2, 2, 2], num_classes)
+    return PreActResNet(PreActBlock, [2, 2, 2, 2], num_classes)
