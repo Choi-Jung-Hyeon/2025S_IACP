1c12bbd7f0c742ce4f2ab5a89d176681be098dbe
Linfer7892 <wnsehd06@g.skku.edu>
2025-07-23 23:55:58 +0900

    Randomcrop

--- datasets.py
+++ datasets.py
@@ -17,7 +17,7 @@ class SSLDataset(Dataset):
 def get_transforms(train=True):
     if train:
         return transforms.Compose([
-            transforms.RandomResizedCrop(32, scale=(0.8, 1.0)),
+            transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
